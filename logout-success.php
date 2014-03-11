8bdbaeadcda6ed0496c8c511368f0f8d83c8dfa0
Jonas Bergler <jonas@meraki.com>
2014-03-11 08:51:10 +0000

    Fix logout continue_url

---
+++ logout-success.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Logout Successful</title>
+
+	<script type="text/javascript" src="assets/jquery-2.1.0.min.js"></script>
+	<link rel="stylesheet" href="assets/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+	<div class="container">
+		<h2>You are now logged out!</h2>
+	</div>
+</body>
+</html>
--- logout.php
+++ logout.php
@@ -1,11 +1,17 @@
 <?php
+	//Default redirect in case we don't have a cookie with a logout url
 	$logoutUrl = "http://www.google.com/";
+
+	//URL for after the logout is complete
+	$continueUrl = "http://" . $_SERVER['SERVER_NAME'] . "/logged-out.php" ;
+
 	if (isset($_COOKIE['LogoutURL'])) {
-		$logoutUrl = urldecode($_COOKIE['LogoutURL']) . "&continue=" . urlencode("http://splash.jb.net.nz/test.php");
+		$logoutUrl = urldecode($_COOKIE['LogoutURL']) . "&continue_url=" . urlencode($continueUrl);
 
 		//Empty cookie, and expire one hour ago so client removes it
 		unset($_COOKIE['LogoutURL']);
 		setcookie("LogoutURL", "", time()-3600);
 	}
+
 	header("Location: {$logoutUrl}");
 
